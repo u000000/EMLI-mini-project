e44246746d307f007600bfa275a37297efb261f2
u000000 <adasko99a@gmail.com>
2024-04-30 23:44:43 +0200

    rain

--- src/RPi/RainDetector/rainDetectorLogic.sh
+++ src/RPi/RainDetector/rainDetectorLogic.sh
@@ -3,12 +3,13 @@
 while true
 do
     if mosquitto_sub -u emli -P raspberry -h localhost -p 1883 -t rain_detector_topic | while read -r mqtt_data; do
-        if [[ $mqtt_data == 'RAINING' ]]; then
-            mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
-            sleep 0.5
-            mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "180"
-            sleep 0.5
-            mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
-        fi
-    done
+            if [[ $mqtt_data == 'RAINING' ]]; then
+                mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
+                sleep 0.5
+                mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "180"
+                sleep 0.5
+                mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
+            fi
+        done
+    fi
 done
