5655255832429226c9e21cb8ab391f14d378959f
u000000 <adasko99a@gmail.com>
2024-04-30 15:29:03 +0200

    rain

--- src/RPi/RainDetector/rainDetectorLogic.sh
+++ src/RPi/RainDetector/rainDetectorLogic.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+while true; do
+    if mosquitto_sub -u emli -P raspberry -h localhost -p 1883 -t rain_detector_topic | while read -r mqtt_data; do
+        if [[ $mqtt_data == 'RAINING' ]]; then
+            mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
+            sleep 0.5
+            mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "180"
+            sleep 0.5
+            mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
+        fi
+    done;
+done
 
-if mosquitto_sub -u emli -P raspberry -h localhost -p 1883 -t rain_detector_topic | while read -r mqtt_data; do
-    if [[ $mqtt_data == 'RAINING' ]]; then
-        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
-        sleep 0.5
-        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "180"
-        sleep 0.5
-        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
-    fi
-done;
--- src/RPi/RainDetector/rainDetectorUART_MQTT.sh
+++ src/RPi/RainDetector/rainDetectorUART_MQTT.sh
@@ -8,31 +8,33 @@ baud_rate="115200"
 exec 3<> "$serial_port"
 stty -F "$serial_port" "$baud_rate"
 
-# Subscribe to MQTT topic
-if mosquitto_sub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic | while read -r mqtt_data; do
-    # Check if data is received via MQTT
-    if [[ $mqtt_data ]]; then
-        # Create JSON object with 'wiper_angle' variable
-        json_data=$(jq -n --arg angle "$mqtt_data" '{"wiper_angle": $angle}')
+while true; do
+    # Subscribe to MQTT topic
+    if mosquitto_sub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic | while read -r mqtt_data; do
+        # Check if data is received via MQTT
+        if [[ $mqtt_data ]]; then
+            # Create JSON object with 'wiper_angle' variable
+            json_data=$(jq -n --arg angle "$mqtt_data" '{"wiper_angle": $angle}')
 
-        # Send JSON object via serial port
-        echo "$json_data" >&3
+            # Send JSON object via serial port
+            echo "$json_data" >&3
+        else
+            # Read data from the serial port
+            while read -r line <&3; do
+                echo "$line"
+
+                if [[ $(echo "$line" | jq -r '.rain_detect') == "1" ]]; then
+                    # Publish information via MQTT
+                    mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t rain_detector_topic -m RAINING
+                fi
+            done
+        fi
+    done;
     else
-        # Read data from the serial port
-        while read -r line <&3; do
-            echo "$line"
-
-            if [[ $(echo "$line" | jq -r '.rain_detect') == "1" ]]; then
-                # Publish information via MQTT
-                mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t rain_detector_topic -m RAINING
-            fi
-        done
+        # Handle the case when mosquitto_sub fails
+        echo "Failed to subscribe to MQTT topic"
     fi
-done;
-else
-    # Handle the case when mosquitto_sub fails
-    echo "Failed to subscribe to MQTT topic"
-fi
+done
 
 # Close the serial port
 exec 3>&-
