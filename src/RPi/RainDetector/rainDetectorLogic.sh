6666794f816dda3c832b009a5f8af1f3e70f5334
u000000 <adasko99a@gmail.com>
2024-05-08 18:33:47 +0200

    raindetectorworks

--- src/RPi/RainDetector/rainDetectorLogic.sh
+++ src/RPi/RainDetector/rainDetectorLogic.sh
@@ -3,10 +3,8 @@
 mosquitto_sub -u emli -P raspberry -h localhost -p 1883 -t rain_detector_topic | while read -r mqtt_data
 do
     if [[ $mqtt_data == 'RAINING' ]]; then
-        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
+        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m 180
         sleep 0.5
-        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "180"
-        sleep 0.5
-        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m "0"
+        mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic -m 0
     fi
 done
--- src/RPi/RainDetector/rainDetectorUART_MQTT.sh
+++ src/RPi/RainDetector/rainDetectorUART_MQTT.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-trap ctrl_c INT
 serial_port="/dev/ttyACM0"
-baud_rate="115200"
 
 exec 3<> "$serial_port"
-stty -F "$serial_port" "$baud_rate"
 
 mosquitto_sub -u emli -P raspberry -h localhost -p 1883 -t wiper_angle_topic > task &
 
@@ -18,12 +15,15 @@ do
     then
             NUMOFLINES=$(wc -l < task)
             mqqt_data=$(tail -n 1 task)
-            json_data="{\"wiper_angle\": $mqqt_data}"
-            echo "$json_data" >&3
+            json_data="{\"wiper_angle\": \"$mqqt_data\"}"
+            echo $json_data >&3
     fi
-
-    serial_data=$(screen serial_port baud_rate)
-    if [[ $(echo "$serial_data" | jq -r '.rain_detect') == "1" ]]; then
+    
+    while ! read -t 1 -u 3; do
+        continue
+    done
+    serial_data=$(cat <&3 | head -n 1)
+    if [[ $serial_data == *"\"rain_detect\": 1"* ]]; then
         mosquitto_pub -u emli -P raspberry -h localhost -p 1883 -t rain_detector_topic -m RAINING
     fi
 done
@@ -33,3 +33,5 @@ function ctrl_c() {
     kill %1
     exit 0
 }
+
+trap ctrl_c INT
